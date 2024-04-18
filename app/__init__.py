8a1514bd2cc8e5115c87ac013fe930795cf29994
Fernanda Ugalde <fernanda.ugalde@uabc.edu.mx>
2024-04-17 17:40:30 -0700

    New commit

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""recognize package"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
-from app import app
-#from flask import Flask # from the flask module import Flask module
+from flask import Flask # from the flask module import Flask module
 
-#app = Flask(__name__)   # create an instance of the Flask class
+app = Flask(__name__)   # create an instance of the Flask class
 
-@app.route("/")   # define decorator that maps view functions to routes
+@app.get("/")   # define decorator that maps view functions to routes
 def index():    # view functiion
     me = {  # dictionary# python dictionary
         "first_name": "Fernanda",   
